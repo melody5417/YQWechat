dfb38e91f60f961771c9de65294bbefb1bdc0b46
yiqiwang(王一棋) <you@example.com>
2017-02-27 21:33:56 +0800

    first commit

---
+++ YQWechat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YQWechat
+//
+//  Created by yiqiwang(王一棋) on 2017/2/27.
+//  Copyright © 2017年 Melody5417. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YQWechat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YQWechat
+//
+//  Created by yiqiwang(王一棋) on 2017/2/27.
+//  Copyright © 2017年 Melody5417. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
