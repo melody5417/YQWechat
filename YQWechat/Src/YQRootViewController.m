1d769c2d474f187c7e0b9d2fb2c015d4b651194d
yiqiwang(王一棋) <you@example.com>
2017-02-28 14:46:57 +0800

    添加tab的四个子控制器

--- YQWechat/Src/YQRootViewController.m
+++ YQWechat/Src/YQRootViewController.m
@@ -8,6 +8,44 @@
 
 #import "YQRootViewController.h"
 
+@interface YQRootViewController ()
+@property (nonatomic, strong) NSArray *childVCArray;
+@end
+
 @implementation YQRootViewController
+
+static YQRootViewController *sharedInstance;
++ (YQRootViewController *)sharedInstance {
+  static dispatch_once_t once;
+  dispatch_once(&once, ^{
+    sharedInstance = [[YQRootViewController alloc] init];
+  });
+  return sharedInstance;
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  // 初始化子控制器
+  [self setViewControllers:self.childVCArray];
+}
+
+#pragma mark - getter & setter
+
+- (NSArray *)childVCArray {
+  if (!_childVCArray) {
+    UIViewController *vc1 = [[UIViewController alloc] init];
+    vc1.tabBarItem.title = @"消息";
+    UIViewController *vc2 = [[UIViewController alloc] init];
+    vc2.tabBarItem.title = @"通讯录";
+    UIViewController *vc3 = [[UIViewController alloc] init];
+    vc3.tabBarItem.title = @"发现";
+    UIViewController *vc4 = [[UIViewController alloc] init];
+    vc4.tabBarItem.title = @"我";
+    
+    _childVCArray = @[vc1, vc2, vc3, vc4];
+  }
+  return _childVCArray;
+}
 
 @end
